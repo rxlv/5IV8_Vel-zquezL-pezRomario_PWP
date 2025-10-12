fde02ca1f53665a9e554b385eee72a33492945b5
rxlv <rvelazquezl2300@alumno.ipn.mx>
2025-10-11 23:29:06 -0600

    TERMINAR FORMULARIO

--- 04Formulario/Js/validacion.js
+++ 04Formulario/Js/validacion.js
@@ -1,16 +1,13 @@
 function validar (formulario) {
-//Validar que el campo no acepte menos de 3 caracteres
 if (formulario.nombre.value.length < 3 ) {
     alert("Por favor escribe más de 3 caracteres en el campo nombre");
     formulario.nombre.focus();
     return false;
 }
-// Validación letras
 var checkStr = formulario.nombre.value;
 var abcOK = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ" + "abcdefghijklmnñopqrstuvwxyz";
 var allvalido = true;
 
-// Comparar cadena de nombre con el resultado de abc
 for (var i = 0; i < checkStr.length; i++) {
     var caracteres = checkStr.charAt(i);
     var letraValida = false;
@@ -31,12 +28,12 @@ if (allvalido == false) {
     return false;
 }
 
-// Validación solo números
+
 var checkStr = formulario.edad.value;
 var numOK = "1234567890";
 var allvalido = true;
 
-// Comparar cadena de número con el numOK
+
 for (var i = 0; i < checkStr.length; i++) {
     var caracteres = checkStr.charAt(i);
     var numeroValido = false;
@@ -56,20 +53,16 @@ if (!allvalido) {
     formulario.edad.focus();
     return false;
 }
-//Validación correo electrónico
+
 var correo = formulario.correo.value;
 var regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 if (!regexCorreo.test(correo)) {
    alert("Ingrese un correo electrónico válido");
    formulario.correo.focus();
    return false;
-//}
 
 } 
 
 
-//Validar correo electrónico que acepte formato texto@texto.texto
-//texto.texto@texto.texto 
-//texto.texto@texto.texto 
 
 }     
